71d6793ea7d471ecccfd7defa51bb037aabcce30
Márcio Saeger <marcio@marciosaeger.com.br>
2019-09-04 17:35:49 -0300

    Parameter bug fix

--- utils/migrationsverket.js
+++ utils/migrationsverket.js
@@ -6,9 +6,9 @@ const migrationsverketStockholm = `${migrationSverketBaseUrl}&bokningstyp=2&enhe
 const migrationsverketUppsala = `${migrationSverketBaseUrl}&bokningstyp=2&enhet=MUP1&sokande=1`
 
 const mvAppointmentRequest = async (url, cityName, beepTimes) => {
-    await request({ url, json: false }, (error, { body }) => {
-        if (error) {
-            console.log('Unable to connect to migrationsverket website!')
+    await request(url, {json: false }, (error, { body }) => {
+        if (error || !body) {
+            console.log('Unable to connect to migrationsverket website or to retrieve it\'s content. Trying again...')
         } else {
             if (body.includes('No time slots available')) {
                 return console.log(`No slots available for ${cityName}`, new Date())
@@ -23,7 +23,7 @@ const checkForSlots = async (time, beepTimes) => {
     await mvAppointmentRequest(migrationsverketStockholm, 'Stockholm', beepTimes)
     await mvAppointmentRequest(migrationsverketUppsala, 'Uppsala', beepTimes)
 
-    setTimeout(() => checkForSlots(), time)
+    setTimeout(() => checkForSlots(time, beepTimes), time)
 }
 
 module.exports = { checkForSlots }
