14f373e07b6e9e2430900f1ed335dbced33b806c
Marcio Saeger <marcio.saeger@digitalroute.com>
2019-11-10 18:18:12 +0100

    Fixing bug when site is under maintenance

--- utils/migrationsverket.js
+++ utils/migrationsverket.js
@@ -2,9 +2,9 @@ const beep = require('beepbeep')
 const request = require('request')
 const dateFormat = require('dateformat')
 
-const migrationSverketBaseUrl = 'https://www.migrationsverket.se/ansokanbokning/valjtyp?sprak=en'
-const migrationsverketStockholm = `${migrationSverketBaseUrl}&bokningstyp=2&enhet=Z209&sokande=1`
-const migrationsverketUppsala = `${migrationSverketBaseUrl}&bokningstyp=2&enhet=MUP1&sokande=1`
+const migrationsverketBaseUrl = 'https://www.migrationsverket.se/ansokanbokning/valjtyp?sprak=en'
+const migrationsverketStockholm = `${migrationsverketBaseUrl}&bokningstyp=2&enhet=Z209&sokande=1`
+const migrationsverketUppsala = `${migrationsverketBaseUrl}&bokningstyp=2&enhet=MUP1&sokande=1`
 
 const mvAppointmentRequest = async (url, cityName, beepTimes) => {
     await request(url, { json: false }, (error, { body }) => {
@@ -13,6 +13,8 @@ const mvAppointmentRequest = async (url, cityName, beepTimes) => {
         } else {
             if (body.includes('No time slots available')) {
                 return console.log(`No slots available for ${cityName} on`, dateFormat(new Date(),"dddd, mmmm dS, yyyy, h:MM:ss TT"))
+            } else if (body.includes('A technical error has unfortunately occurred')) {
+                return console.log(`Website is under maintenance for ${cityName}`)
             }
             beep(beepTimes)
             console.log(url)
